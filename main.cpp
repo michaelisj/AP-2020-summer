dabb24fe47737984e4670c26674c2285e421403b
Michael Itzhaki <michaelitzhaki@gmail.com>
2020-07-15 22:21:28 +0300

    Added debuggin options

--- main.cpp
+++ main.cpp
@@ -1,9 +1,21 @@
+#include <cstdint>
 #include <iostream>
+
+
+std::uint64_t fibonacci_number(const std::uint16_t index)
+{
+    if (index == 0 || index == 1) {
+        return 1;
+    }
+
+    return fibonacci_number(index - 1) + fibonacci_number(index - 2);
+}
 
 int main(int, char**) {
     std::cout << "Hello, world!\n";
 
     std::cout << "Using an automatic build now!\n";
+    std::cout << "The tenth fibonacci number is: " << fibonacci_number(10) << std::endl;
 
     return 0;
 }
