1efea6d75a776e0ebebab5870631d76660862c66
Michael Itzhaki <michaelitzhaki@gmail.com>
2020-07-16 21:12:45 +0300

    Added linux commands to the program

---
+++ Utilities/Traits.hpp
@@ -0,0 +1,4 @@
+#pragma once
+
+
+constexpr auto NUMBER_OF_TESTS = 20;
--- main.cpp
+++ main.cpp
@@ -1,15 +1,31 @@
 #include <cstdint>
 #include <iostream>
+#include <filesystem>
+
+#include <unistd.h>
 
 #include "Utilities/Utilities.hpp"
+#include "Utilities/Traits.hpp"
 
 
 
 int main(int, char**) {
+    const auto path = std::filesystem::absolute(".");
+
+    std::cout << "Current path is: " << path.string() << std::endl;
     constexpr auto me = "Michael";
 
     char array[10];
     example::utilities::copy_string(array, me);
+
+    std::cout << "Number of tests: " << NUMBER_OF_TESTS << std::endl;
+
+    const pid_t childId = fork();
+    if (childId == 0) {
+        std::cout << "In child!" << std::endl;
+    } else {
+        std::cout << "In parent!" << std::endl;
+    }
 
     std::cout << array << std::endl;
 
